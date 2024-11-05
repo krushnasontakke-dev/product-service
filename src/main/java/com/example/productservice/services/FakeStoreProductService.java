f005e10d827a20c5a3d9437f9800226d0f74124c
Krushna Sontakke <krushna.sontakke06@gmail.com>
2024-11-05 17:01:55 +0530

    Http call using RestTemplate

--- src/main/java/com/example/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/example/productservice/services/FakeStoreProductService.java
@@ -1,13 +1,40 @@
 package com.example.productservice.services;
 
+import com.example.productservice.dtos.FakeStoreProductDto;
+import com.example.productservice.models.Category;
 import com.example.productservice.models.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FakeStoreProductService implements ProductService{
+    RestTemplate restTemplate;
+
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public Product getProductById(Long id) {
-        return null;
+        FakeStoreProductDto  fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
+        return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+    }
+
+    private Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
+        if (fakeStoreProductDto == null) {
+            return null;
+        }
+
+        Product product = new Product();
+        product.setId(fakeStoreProductDto.getId());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDesc(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+
+        Category category = new Category();
+        category.setTitle(fakeStoreProductDto.getCategory());
+        product.setCategory(category);
+
+        return product;
     }
 }
